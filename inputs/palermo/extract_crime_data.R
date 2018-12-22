edd30c4ee00f46a9d35c2d09346c7a00bac9c424
mariopaolucci <mario.paolucci@gmail.com>
2018-12-22 22:22:47 +0100

    produced age range file too
    Phew.

--- inputs/palermo/extract_crime_data.R
+++ inputs/palermo/extract_crime_data.R
@@ -27,27 +27,20 @@ df <-
   read_delim(" ") %>%
   select(c(1,4,3)) %>%
   rename("male?" = 1, "age" = 3, "p" = 2) %>%
+  mutate(age = str_replace_all(age,"\\<|\\+", "")) %>%
+  separate(age, into = c("age_from", "age_to"), sep = "-") %>%
   mutate(
-    age_from =
-      age %>%
-      str_extract("\\d+") %>%
-      map(as.numeric),
-    age_to = 
-      age %>%
-      ifelse(length(str_extract_all(.,"\\d+")) > 1, str_extract_all(.,"\\d+")[[2]], ""),
-    `male?` = if_else(`male?`=="Female",FALSE, TRUE)
-  )
-
-
-df <-
-  file.path("raw", "CrimeYearlyByGenderAgePalermo.csv") %>%
-  read_delim(" ") %>%
-  select(c(1,4,3)) %>%
-  rename("male?" = 1, "age" = 3, "p" = 2) %>%
-  mutate(
-    age_to = age %>%
-      length(map(.,function(x)str_extract_all(x,"\\d+")))
-
-  )
-
+    age_to = map(age_to,as.numeric),
+    age_from = as.numeric(age_from),
+    age_to = case_when(
+      age_from == 13 ~ 13,
+      age_from == 65 ~ 200,
+      TRUE ~ age_from
+    ),
+    age_from = case_when(
+      age_from == 13 ~ 0,
+      TRUE ~ age_from
+    )
+  ) %>%
+  write_csv(file.path("data", "crime_rate_by_gender_and_age_range.csv"))  
 
