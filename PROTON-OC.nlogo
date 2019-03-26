e214ba2441f525d66952dc394fc7ed7b4119636d
mariopaolucci <mario.paolucci@gmail.com>
2019-03-26 11:01:08 +0100

    tests added

--- PROTON-OC.nlogo
+++ PROTON-OC.nlogo
@@ -848,50 +848,6 @@ to commit-crimes
   ]
 end
 
-; called only by tests - will be removed later
-to crime-stats
-  foreach table:keys c-range-by-age-and-sex [ cell ->
-    let value last table:get c-range-by-age-and-sex cell
-    show (word
-      first cell ", "
-      last cell ", "
-      first value ", "
-      (last value * ticks) ", "
-      (mean [ num-crimes-committed ] of all-persons with [
-        age > last cell and age <= first value and male? = first cell
-      ])
-    )
-  ]
-end
-
-; called only by tests - will be removed later
-to crime-stats-go-2
-  while [
-    reduce [ [ i j ] -> i and j ] runresult [
-      ->  map [
-        cell ->
-        ifelse-value ((any? all-persons with [
-          age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-        ]) and (sum [ num-crimes-committed ] of all-persons with [
-          age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-        ] > 0))
-        [
-          abs last last table:get c-range-by-age-and-sex cell * ticks -
-          (mean [ num-crimes-committed ] of all-persons with [
-            age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-          ] ) < standard-deviation [ num-crimes-committed ] of all-persons with [
-            age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-          ]
-        ] [
-          true
-        ]
-      ] table:keys c-range-by-age-and-sex
-    ]
-  ] [
-    go
-  ]
-end
-
 to-report arrest-probability-with-intervention [ group ]
   if-else (intervention-on? and OC-members-scrutinize? and any? group with [ oc-member? ]) [
     report probability-of-getting-caught * oc-arrest-multiplier
--- proton-oc-tests/src/test/scala/it/cnr/istc/labss/proton/oc/tests/OCCrimeStatsTest.scala
+++ proton-oc-tests/src/test/scala/it/cnr/istc/labss/proton/oc/tests/OCCrimeStatsTest.scala
@@ -8,34 +8,50 @@ class OCCrimeStatsTest extends OCModelSuite {
 
   test("Stats on crime accurate to one SD") { ws =>
     ws.cmd("""
-      set num-persons 2000
+      set num-persons 1500
       setup
       """)
     for (fid <- 1 to 20) {
-      println(fid)
       ws.cmd("go")
       ws.rpt("""
-      reduce [ [ i j ] -> i and j ] runresult [
-      ->  map [
-        cell ->
-        ifelse-value ((any? all-persons with [
-          age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-        ]) and (sum [ num-crimes-committed ] of all-persons with [
-          age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-        ] > 0))
-        [
-          abs last last table:get c-range-by-age-and-sex cell * ticks -
-          (mean [ num-crimes-committed ] of all-persons with [
-            age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-          ] ) < standard-deviation [ num-crimes-committed ] of all-persons with [
-            age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
-          ]
-        ] [
-          true
+        reduce [ [ i j ] -> i and j ] runresult [
+          ->  map [ 
+            cell ->
+            ifelse-value ((any? all-persons with [
+              age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
+            ]) and (sum [ num-crimes-committed ] of all-persons with [
+              age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
+            ] > 0)) [
+              abs last last table:get c-range-by-age-and-sex cell * ticks -
+              (mean [ num-crimes-committed ] of all-persons with [
+              age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
+            ] ) < 2 * standard-deviation [ num-crimes-committed ] of all-persons with [
+              age > last cell and age <= first last table:get c-range-by-age-and-sex cell and male? = first cell
+            ]
+            ] [
+              true
+            ]
+          ] table:keys c-range-by-age-and-sex
         ]
-      ] table:keys c-range-by-age-and-sex
-    ] any? persons with [ any? job-link-neighbors and age < 18 ] ") shouldBe false
-    """) shouldBe true
+      """) shouldBe true
+      println(fid)
     } 
   } 
 }
+/* helper that could be reintroduced in case of need
+to crime-stats
+  foreach table:keys c-range-by-age-and-sex [ cell ->
+    let value last table:get c-range-by-age-and-sex cell
+    show (word
+      first cell ", "
+      last cell ", "
+      first value ", "
+      (last value * ticks) ", "
+      (mean [ num-crimes-committed ] of all-persons with [
+        age > last cell and age <= first value and male? = first cell
+      ])
+    )
+  ]
+end
+*/
+
