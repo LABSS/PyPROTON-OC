56bef2e65953a1b72342cef1be571bededb2e736
Mario <mario.paolucci@gmail.com>
2019-09-19 13:56:57 +0200

    Preparing rp0.7

---
+++ experiments-xml/prettyprint.py
@@ -0,0 +1,18 @@
+#pretty print method
+def indent(elem, level=0):
+    i = "\n" + level*"  "
+    j = "\n" + (level-1)*"  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for subelem in elem:
+            indent(subelem, level+1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = j
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = j
+    return elem
+
