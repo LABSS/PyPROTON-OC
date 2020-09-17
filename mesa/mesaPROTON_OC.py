0f49447122a5075873a3d7cfeb19b4f173b1a4b0
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-09-17 11:40:36 +0200

    Added setup_facilitators

--- mesa/mesaPROTON_OC.py
+++ mesa/mesaPROTON_OC.py
@@ -100,6 +100,7 @@ class MesaPROTON_OC(Model):
         self.oc_members_scrutinize = False
         self.facilitator_repression = False
         self.facilitator_repression_multiplier = 2.0
+        self.percentage_of_facilitators = 0.005
 
         # Folders definition
         self.mesa_dir = os.getcwd()
@@ -166,8 +167,8 @@ class MesaPROTON_OC(Model):
                 x.job_level = 2 if self.rng.uniform(0, 1) < ratio_on else 0
 
     def setup_facilitators(self):
-        for x in self.schedule.agents:
-            x.facilitator = True if not x.oc_member and x.age() > 18 and (self.rng.uniform(0, 1) < self.percentage_of_facilitators) else False
+        for agent in self.schedule.agents:
+            agent.facilitator = True if not agent.oc_member and agent.age() > 18 and (self.rng.uniform(0, 1) < self.percentage_of_facilitators) else False
 
     def read_csv_city(self, filename):
         return pd.read_csv(os.path.join(self.data_folder, filename + ".csv"))
@@ -723,6 +724,7 @@ class MesaPROTON_OC(Model):
         self.calculate_criminal_tendency()
         self.calculate_arrest_rate()
         self.setup_oc_groups()
+        self.setup_facilitators()
 
     def assign_jobs_and_wealth(self):
         """
