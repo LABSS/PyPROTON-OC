6f1840f6639edfdf20b5a74483bda7a1a1e0d38e
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-10-16 10:36:58 +0200

    Minor changes

--- mesa/Person.py
+++ mesa/Person.py
@@ -131,14 +131,12 @@ class Person(Agent):
         :param asker: agent
         :return: None
         """
-        if type(asker) == list:
-            for person in asker:
-                self.neighbors.get("professional").add(person)
-                person.neighbors.get("professional").add(self)
-        else:
-            self.neighbors.get("professional").add(asker)
-            asker.neighbors.get("professional").add(self)
-        
+        if type(asker) != list:
+            asker = [asker]
+        for person in asker:
+            self.neighbors.get("professional").add(person)
+            person.neighbors.get("professional").add(self)
+
     def addSiblingLinks(self, targets):
         for x in targets:
             if x != self:
