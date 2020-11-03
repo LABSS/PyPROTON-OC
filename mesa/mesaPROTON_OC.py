620e071c56378f4f48fa129fd967b9347a827291
Francesco Mattioli <Francesco.mttl@gmail.com>
2020-11-03 15:11:41 +0100

    Minor changes

--- mesa/mesaPROTON_OC.py
+++ mesa/mesaPROTON_OC.py
@@ -179,7 +179,7 @@ class MesaPROTON_OC(Model):
 
     def setup_facilitators(self):
         for agent in self.schedule.agent_buffer(shuffled=True):
-            agent.facilitator = True if not agent.oc_member and agent.age() > 18 and (self.rng.uniform(0, 1) < self.percentage_of_facilitators) else False
+            agent.facilitator = True if not agent.oc_member and agent.age() > 18 and (self.rng.uniform(0, 1) < self.likelihood_of_facilitators) else False
 
     def read_csv_city(self, filename):
         return pd.read_csv(os.path.join(self.data_folder, filename + ".csv"))
