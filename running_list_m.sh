c4439bf11504435cc82eaa8fbccc9176e81f4b9b
Francesco Mattioli <Francesco.mttl@gmail.com>
2021-02-15 10:03:43 +0100

    removed files not relevant to the version

--- running_list_m.sh
+++
@@ -1,8 +0,0 @@
-export NLOGO=/home/mario/NetLogo\ 6.1.1/netlogo-headless.sh
-#export NLOGO=/home/paolucci/NetLogo\ 6.1.1/netlogo-headless.sh
-while read p; do
-  echo "$p"
-  EXP=$p
-  time "$NLOGO" --model PROTON-OC.nlogo --setup-file experiments-xml/$EXP --table $EXP.`hostname`.`git rev-parse --short HEAD`.csv > $EXP.`hostname`.`git rev-parse --short HEAD`.out 2>&1
-  #sleep 1s
-done < experiments-xml/missing
