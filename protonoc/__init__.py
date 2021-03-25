d0e907ccd0dfd06f102f3d43f29c8fc33bd32b75
Francesco Mattioli <Francesco.mttl@gmail.com>
2021-03-25 19:25:16 +0100

    update setup.py

--- protonoc/__init__.py
+++ protonoc/__init__.py
@@ -1,3 +1,6 @@
 from protonoc.simulator.model import ProtonOC
 from protonoc.simulator.entities import Person, School, Job, Employer
 from protonoc.simulator import utils
+
+#version
+__version__ = "1.0.0"
