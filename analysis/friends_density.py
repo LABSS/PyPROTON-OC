09e69b7036b1486c78bd02fcdd70f5b5befb18c6
Francesco Mattioli <Francesco.mttl@gmail.com>
2021-05-03 14:55:09 +0200

    analysis dense friendship

--- analysis/friends_density.py
+++ analysis/friends_density.py
@@ -39,12 +39,50 @@ map = {'count persons with [ migrant? ]' : "number_migrants",
        'count jobs' : "number_jobs",
        "[step]":"tick"}
 
+PATH_PYTHON = "E:\\proton\\dense_friends\\res" #YOUR PYTHON PATH HERE
+PATH_NETLOGO = "E:\\proton\\comparative\\panel_abm_all.csv" #YOUR NETLOGO PATH HERE
 
-PATH_PYTHON = "E:\\proton\\dense_friends\\res"
+interventions = ['baseline', 'disruptive', 'facilitators', 'preventive', 'students']
+data_netlogo = dict()
+data_python = dict()
+
+for interv in pd.read_csv(PATH_NETLOGO).groupby("intervention"):
+    data_netlogo[interv[0]] = interv[1]
 
 all_data = list()
 for index_run, run_file in enumerate(os.scandir(PATH_PYTHON)):
-    out_data = pd.read_pickle(run_file.path)
-    out_data["run_id"] = [index_run for n in range(361)]
-    all_data.append(out_data)
-total_python = pd.concat(all_data, ignore_index=True)
+    out_data = pd.read_pickle(run_file.path)[0:361]
+    if out_data.intervention[0] == "baseline":
+        out_data["run_id"] = [index_run for n in range(361)]
+        all_data.append(out_data)
+
+
+fig, axs = plt.subplots(1, 8, figsize=(80, 20))
+for index, run in enumerate(all_data):
+    axs[index].plot(run.current_oc_members)
+    axs[index].set_title(str(index))
+
+# for index, run in enumerate(all_data):
+#     axs[index+8].plot(run.tot_friendship_link)
+#     axs[index+8].set_title(str(index))
+
+plt.savefig("dense_frieds.png")
+
+
+
+# total_python = pd.concat(all_data, ignore_index=True)
+
+# for interv in total_python.groupby("intervention"):
+#     data_python[interv[0]] = interv[1]
+
+# superrun_python = dict()
+# superrun_netlogo = dict()
+# for interv in interventions:
+#     reporter_python = data_python[interv][map.values()]
+#     superrun_python[interv] = pd.concat([tick.mean(axis=0) for number_tick, tick in
+#                                          reporter_python.groupby("tick")], axis=1).T
+#
+#     reporter_netlogo = data_netlogo[interv][map.keys()]
+#
+#     superrun_netlogo[interv] = pd.concat([tick.mean(axis=0) for number_tick, tick in
+#                                          reporter_netlogo.groupby(["[step]"])], axis=1).T
